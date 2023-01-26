1957d0295a1ef56f14690e4de50bcfd5212755e8
N0mekop <blinkevin@hotmail.com>
2023-01-26 15:04:44 +0100

    Encore des test de pagination (CPT)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,5 +26,20 @@ class ProductController extends AbstractController
         return $this->render('product/index.html.twig', [
             'product' => $product,
         ]);
+    }
+
+    public function listAction(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request)
+    {
+        $dql   = "SELECT a FROM AcmeMainBundle:Article a";
+        $query = $em->createQuery($dql);
+
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            2 /*limit per page*/
+        );
+
+        // parameters to template
+        return $this->render('category/list.html.twig', ['pagination' => $pagination]);
     }
 }
